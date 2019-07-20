a34785398dffda10d6988d24e78ba80a3299ebd8
Ezekiel05 <44927763+Ezekiel05@users.noreply.github.com>
2019-07-21 01:35:52 +0800

    Android App UI Desgin
    Uploading UI design for consolidation

---
+++ Shopping/app/src/main/java/com/example/shopping/Login.java
@@ -0,0 +1,10 @@
+package com.example.shopping;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    override function OnCreate(saveinstancestate:bundle){
+
+    }
+}
