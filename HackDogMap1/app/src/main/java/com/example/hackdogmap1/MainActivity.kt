5bcd5fb76ee8fb9bc9798edaf6c7822a882a2ff7
Ezekiel05 <44927763+Ezekiel05@users.noreply.github.com>
2019-07-21 00:37:07 +0800

    Hackdog Map
    Cretaed Grocery Map

---
+++ HackDogMap1/app/src/main/java/com/example/hackdogmap1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.hackdogmap1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import android.widget.Button.generateViewId
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
